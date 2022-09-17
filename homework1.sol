e43cc01957db9562c880a3b355a73a7bd58d2f7b
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-09-17 21:28:54 +0300

    Update homework1.sol

--- homework1.sol
+++ homework1.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.7;
 // SPDX-License-Identifier: MIT
 
 contract FeeCollector { 
+ // Define the state variables for the owner and balance.
     address public owner;
     uint256 public balance;
     
@@ -10,16 +11,17 @@ contract FeeCollector {
     }
     
     receive() payable external {
-     /* Contract accept the ethers and deposit balance.*/
+     // Contract accept the ethers and deposit balance.
         balance += msg.value; 
     }
     
     
     function withdraw(uint amount, address payable destAddr) public {
-      /* Controls the if msg.sender is "owner"*/
+      // Use require to only allow the owner to withdraw
         require(msg.sender == owner, "Only owner can withdraw");
         require(amount <= balance, "Insufficient funds");
         
+        // Define destAddr as payable to transact.
         destAddr.transfer(amount);
         balance -= amount;
     }
