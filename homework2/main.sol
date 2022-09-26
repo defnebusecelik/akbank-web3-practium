99f684d88c08c76ca5fc96a3012119d70c4095a0
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-09-26 23:18:17 +0300

    Update main.sol

--- homework2/main.sol
+++ homework2/main.sol
@@ -6,28 +6,22 @@ contract Todos {
         string text;
         bool completed;
     }
-
-    // An array of 'Todo' structs
+    
     Todo[] public todos;
 
     function create(string calldata _text) public {
-        // 3 ways to initialize a struct
-        // - calling it like a function
         todos.push(Todo(_text, false));
 
-        // key value mapping
+        //mapping
         todos.push(Todo({text: _text, completed: false}));
 
         // initialize an empty struct and then update it
         Todo memory todo;
         todo.text = _text;
-        // todo.completed initialized to false
 
         todos.push(todo);
     }
 
-    // Solidity automatically created a getter for 'todos' so
-    // you don't actually need this function.
     function get(uint _index) public view returns (string memory text, bool completed) {
         Todo storage todo = todos[_index];
         return (todo.text, todo.completed);
